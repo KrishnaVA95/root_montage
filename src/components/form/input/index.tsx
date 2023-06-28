87c9f6222734da969a9ff8a39ba672a4d3eec9f6
Krishna <krishnavarela.a@gmail.com>
2023-06-28 12:46:03 -0300

    feat: logic login

--- src/components/form/input/index.tsx
+++ src/components/form/input/index.tsx
@@ -1,15 +1,19 @@
 import styles from "./styles.module.scss"
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+import { forwardRef, InputHTMLAttributes } from "react";
 
-interface IImputOutlined {
-    type: string;
+interface IImputOutlined extends InputHTMLAttributes<HTMLInputElement> {
+    id: string;
+    type: "text" | "password" | "email";
     label: string;
+    register: UseFormRegisterReturn<string>;
 }
 
-export default async function ImputOutlined ({type, label}: IImputOutlined){
+export default async function ImputOutlined ({id, register, type, label}: IImputOutlined){
 
     return(
         <div className={styles.container}>
-            <input type={type} required/>
+            <input type={type} required id={id}  {...register}/>
             <span>{label}</span>
         </div>
     ) 
--- src/components/form/loginForm/index.tsx
+++ src/components/form/loginForm/index.tsx
@@ -1,19 +1,34 @@
+'use client'
+import {useForm, SubmitHandler} from 'react-hook-form'
 import ImputOutlined from "../input"
 import styles from "./styles.module.scss"
-
-
+// import { useContext } from 'react';
+export interface iLoginFormData {
+    email: string;
+    password: string;
+  }
 
 export default async function FomrLogin (){
 
+    const { register, handleSubmit, reset, formState: { errors }  } = useForm<iLoginFormData>({
+    })
+    
+    // const { userLogin } = useContext(UserContext)
+    
+    const submit: SubmitHandler<iLoginFormData> = (data) =>{
+        console.log(data)
+        // userLogin(data)
+        reset()
+    }
+
     return(
         <div className={styles.container}>
-            <form action="">
+            <form onSubmit={handleSubmit(submit)}>
                 <h2>Sign <span>in</span></h2>
-                <ImputOutlined type="text" label='Email'/>
-                <ImputOutlined type="password" label='Password'/>
+                <ImputOutlined id="email" type="text" label='Email' register={register('email')}/>
+                <ImputOutlined id="password" type="password" label='Password'  register={register('password')}/>
                 <input type="submit" value='Login'/>
             </form>
         </div>
     ) 
-        
 }
