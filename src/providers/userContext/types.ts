3ccd96b2718d24ccb20f80ee18b88b7a9cdcfa79
Krishna <krishnavarela.a@gmail.com>
2023-06-28 11:31:40 -0300

    remuve provider user

--- src/providers/userContext/types.ts
+++ src/providers/userContext/types.ts
@@ -1,26 +1,26 @@
 
 
-export interface iUser{
-    id: number;
-    name: string;
-    email: string;
-}
+// export interface iUser{
+//     id: number;
+//     name: string;
+//     email: string;
+// }
 
-// Resposta da api
-export interface iUserResponse {
-    accessToken: string;
-    user: iUser;
-}
-
-
-export interface iUserContextProps{
-    children: React.ReactNode;
-}
+// // Resposta da api
+// export interface iUserResponse {
+//     accessToken: string;
+//     user: iUser;
+// }
 
 
-export interface iUserContext{ 
-    user: iUser | null;
-    setUser: React.Dispatch<React.SetStateAction<iUser | null>>;
-    userLogin: (data: iLoginFormData) => Promise<void>;
-    userLogout: () => void
-}
+// export interface iUserContextProps{
+//     children: React.ReactNode;
+// }
+
+
+// export interface iUserContext{ 
+//     user: iUser | null;
+//     setUser: React.Dispatch<React.SetStateAction<iUser | null>>;
+//     userLogin: (data: iLoginFormData) => Promise<void>;
+//     userLogout: () => void
+// }
